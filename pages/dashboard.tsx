07f5ebe4958ae9be381f016d8f0bcc48886612ab
inmare78 <office@timejet.de>
2025-05-25 09:33:32 +0200

    Create dashboard.tsx

---
+++ pages/dashboard.tsx
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+  return (
+    <main style={{ padding: '2rem' }}>
+      <h1>Dashboard</h1>
+      <p>Hier siehst du später deine Rechnungen 🧾</p>
+    </main>
+  )
+}
